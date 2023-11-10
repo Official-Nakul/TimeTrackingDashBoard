41f2e1327cfaf9dec6a1306a2b94858a59433493
Official-Nakul <124872597+Official-Nakul@users.noreply.github.com>
2023-11-10 11:40:57 +0530

    Time Tracking Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import "./App.css";
 import TaskCard from "./component/TaskCard/TaskCard";
+import userpfp from "./assets/images/image-jeremy.png";
 function App() {
   return (
     <>
-      <TaskCard />
+      <div className="container">
+        <div className="details">
+          <div className="userDetailsContainer">
+            <div className="pfp">
+              <img src={userpfp} alt="" />
+            </div>
+            <div className="data">
+              <p className="report">Report for</p>
+              <p className="name">
+                Jeremy <br /> Robson
+              </p>
+            </div>
+          </div>
+          <div className="timeline">
+            <p className="dialy">Daily</p>
+            <p className="weekly">Weekly</p>
+            <p className="montly">monthly</p>
+          </div>
+        </div>
+
+        <TaskCard />
+      </div>
     </>
   );
 }
